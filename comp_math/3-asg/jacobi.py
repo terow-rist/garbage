f79a58e4270127bf5db05e2f5215b907889abf49
missayev <feeerhd.342@gmail.com>
2025-01-13 02:20:34 +0500

    refactor: comp math/jacobi added error's graph

--- comp_math/3-asg/jacobi.py
+++ comp_math/3-asg/jacobi.py
@@ -14,7 +14,7 @@ matrix = []
 for i in range(0, n):
     print("Enter {}'s list.".format(i+1))
     if i == 0:
-        print("Example: 1,2..n = [1, 2..n]")
+        print("Example: 1 2 ... n = [1, 2..n]")
     matrix.append([int(j) for j in input().split()])
 
 should_be, c = len(matrix[0]), 1
@@ -41,7 +41,7 @@ for i in matrix:
 
 # Iteration proccess
 c = 0
-result = []
+result, errors = [], []
 converged = False
 while not converged:
     new_x_list = []
@@ -50,12 +50,14 @@ while not converged:
         new_x_list.append(float(exp.subs(res)))
     
     converged = all(abs(new_x_list[i] - x_list[i]) < e for i in range(len(x_list)))
+    errors.append([abs(new_x_list[i] - x_list[i]) for i in range(len(x_list))])
     result.append(x_list)
     x_list = new_x_list
     c+=1
 
 # Start for plotting
 transposed_result = list(zip(*result))  
+err_result = list(zip(*errors))
 iterations = [i for i in range(c)]
 
 for i, values in enumerate(transposed_result):
@@ -68,4 +70,13 @@ plt.ylabel("X's Roots")
 plt.legend()
 plt.show()
 
+for i, values in enumerate(err_result):
+    plt.plot(iterations, values, marker="o", label=f"x{i+1}")  
+
+plt.title("Errors of Jacobi method.")
+plt.grid()
+plt.xlabel("Iterations")
+plt.ylabel("X's Roots")
+plt.legend()
+plt.show()
 
