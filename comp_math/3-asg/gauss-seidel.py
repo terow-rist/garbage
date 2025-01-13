ddf113d5f823fdb9085990a687d41c783debc4dd
missayev <feeerhd.342@gmail.com>
2025-01-13 10:21:14 +0500

    feat: gauss-seidel and matrix logic

--- comp_math/3-asg/gauss-seidel.py
+++ comp_math/3-asg/gauss-seidel.py
@@ -13,36 +13,35 @@ matrix = [
 ]
 # matrix = matrix_logic.matrix_init(n, m)
 
-# Values of each Xi in matrix 
-# Initialize symbols for sympy
-x_list = [-2.4, 5.0, 1] # or [0]*len(matrix)
+# Initial values of each Xi in the matrix
+x_list = [-2.4, 5.0, 1]  # Initial guesses
 
+# Initialize symbols for sympy
 x_symbols = symbols('x1:'+str(len(matrix)+1))
 
 # Create expression for each variable
 c = 0
 expressions = [] 
 for i in matrix:
-    expr = sum([coef*var for coef, var in zip(i, x_symbols)]) - i[-1]
+    expr = sum([coef * var for coef, var in zip(i[:-1], x_symbols)]) - i[-1]
     expressions.append(solve(expr, x_symbols[c])[0])
-    # print(solve(expr, x_symbols[c]))
-    c+=1
+    c += 1
 
-# Iteration proccess
+# Iteration process
 c = 0
 result, errors = [], []
 converged = False
 while not converged:
-    new_x_list = []
-    for exp in expressions:
-        res = {x_symbols[i]: x_list[i] for i in range(len(matrix))}
-        new_x_list.append(float(exp.subs(res)))
-    
+    new_x_list = x_list[:]
+    for j, exp in enumerate(expressions):
+        res = {x_symbols[k]: new_x_list[k] for k in range(len(matrix))}  # Use updated values
+        new_x_list[j] = float(exp.subs(res))
+
     converged = all(abs(new_x_list[i] - x_list[i]) < e for i in range(len(x_list)))
     errors.append([abs(new_x_list[i] - x_list[i]) for i in range(len(x_list))])
     result.append(x_list)
     x_list = new_x_list
-    c+=1
+    c += 1
 
 # Start for plotting
 transposed_result = list(zip(*result))  
@@ -52,7 +51,7 @@ iterations = [i for i in range(c)]
 for i, values in enumerate(transposed_result):
     plt.plot(iterations, values, marker="o", label=f"x{i+1}")  
 
-plt.title("Jacobi method.")
+plt.title("Gauss-Seidel Method")
 plt.grid()
 plt.xlabel("Iterations")
 plt.ylabel("X's Roots")
@@ -62,10 +61,9 @@ plt.show()
 for i, values in enumerate(err_result):
     plt.plot(iterations, values, marker="o", label=f"x{i+1}")  
 
-plt.title("Errors of Jacobi method.")
+plt.title("Errors of Gauss-Seidel Method")
 plt.grid()
 plt.xlabel("Iterations")
-plt.ylabel("X's Roots")
+plt.ylabel("Errors")
 plt.legend()
 plt.show()
-
