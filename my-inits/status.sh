a8a35d464315cc88ca6b8e8aaf6e4a69d6eac517
missayev <feeerhd.342@gmail.com>
2025-04-30 11:35:43 +0500

    add: dwm-status.sh

---
+++ my-inits/status.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+DELIM=" | "
+
+while true; do
+    bl=$(acpi | awk -F, '{print $2}')
+
+    datetime=$(date '+%b %d (%a) %I:%M%p')
+
+    xsetroot -name "[BL: $bl$DELIM$datetime]"
+
+    sleep 30
+done
+
