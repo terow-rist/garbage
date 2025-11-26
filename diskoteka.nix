faff95077821e857d5d4d1ac31baa50a2a97534b
terow-rist <terow-rist@totatato.com>
2025-11-26 18:17:09 +0500

    add: diskoteka.nix

---
+++ diskoteka.nix
@@ -0,0 +1,54 @@
+{
+  disko.devices = {
+    disk.diskoteka= {
+      device = "/dev/sda"; #change it!
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+
+          esp = {
+            name = "ESP";
+            size = "256M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
+          };
+          
+        };
+      };
+    };
+
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+        };
+      };
+    };
+
+  };
+}
